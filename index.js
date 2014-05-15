bd0b6d49c936461bb8373261faf353e9e72f4884
haylee <hayleejung2@gmail.com>
2014-05-15 03:31:32 -0400

    first

--- index.js
+++ index.js
@@ -1,37 +1,23 @@
-/*
-	restToSerial
-	a node.js app to read take requests and send as serial data
-	requires:
-		* node.js (http://nodejs.org/)
-		* express.js (http://expressjs.com/)
-		* socket.io (http://socket.io/#how-to-use)
-		* serialport.js (https://github.com/voodootikigod/node-serialport)
-		
-	based on the core examples for socket.io and serialport.js
-		
-	created 5 Nov 2012
-	modified 19 Jan 2014
-	by Tom Igoe
-	
-*/
-
-
 var serialport = require("serialport"),		// include the serialport library
 	SerialPort  = serialport.SerialPort,	   // make a local instance of serial
 	express = require('express'),				   // make an instance of express
 	open = require('open'),                   // used to open the browser
-	url = 'http://latexpixel.herokuapp.com/';            // URL to open in the browser
+	url = 'http://localhost:8080',            // URL to open in the browser
+	// WebSocketServer = require('ws').Server,
+	// http = require('http');
 	
 var app = express(),								   // start Express framework
    server = require('http').createServer(app);		// start an HTTP server
 
 // configure server to serve static files from /js so you can use zepto:
 app.use('/js', express.static(__dirname + '/js'));
+
+app.use(express.static(__dirname + '/public/'));
  
 // third word of the command line is serial port name:
-console.log('hello', process.argv);
-var portName = process.argv[2];	
-// var portName = "/dev/tty.usbmodem1431";
+// console.log('hello', process.argv);
+// var portName = process.argv[2];	
+var portName = "/dev/tty.usbmodem1431";
 
 // print out the port you're listening on:
 console.log("opening serial port: " + portName);	
@@ -61,13 +47,13 @@ app.get('/', function (request, response) {
 // take anything that begins with /output:
 app.get('/output/*', function (request, response) {
   // the route is the first parameter of the URL request:
-  var brightnessCommand = request.params[0];  
-  console.log("received "+ brightnessCommand);
+  var latexCommand = request.params[0];  
+  console.log("received "+ latexCommand);
 
   // send it out the serial port:
-  myPort.write(brightnessCommand);
+  myPort.write(latexCommand);
   // send an HTTP header to the client:
   response.writeHead(200, {'Content-Type': 'text/html'});
   // send the data and close the connection:
-  response.end(brightnessCommand);
+  response.end(latexCommand);
 });
